e13ba2d279bffb3e5c3fb26b7e6c084800eee2d5
Ulysse94 <32864392+Ulysse94@users.noreply.github.com>
2018-09-25 19:23:50 +0200

    Next arrow effects

--- scripts/app.js
+++ scripts/app.js
@@ -2,14 +2,18 @@ $(document).ready(function() {
 	// On écoute le scroll sur la page
 	$(document).scroll(function() {
 		// On récupère la hauteur de la page // On récupère la position du scroll // On calcule l'endroit à partir duquel on veut faire apparaître le bouton
-		let heightWindow = $(window).height(), scroll = $(window).scrollTop(), position = heightWindow - scroll;
+		let heightWindow = $(window).height(), scroll = $(window).scrollTop(), position = heightWindow - scroll, value = 100/scroll ;
 
-		if (position > 0) {
-			// On fait apparaître le bloc en fondu
-			$('#next').fadeIn(2000);
-		} else {
-			// On fait disparaitre le bloc en fondu
-			$('#next').fadeOut(500);
-		}
+        if (value > 0.25) { 
+            $('#next').css("opacity", value);
+        }
+        else {
+            // On fait disparaitre le bloc en fondu
+            $('#next').fadeOut(1000);
+        }
+        if (position == heightWindow) { 
+            $('#next').fadeIn(1000);
+        }
+        
 	});
 });
